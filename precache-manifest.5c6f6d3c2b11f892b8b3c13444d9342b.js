83abcdfe17d9983bd89be4aefb134a4caaf71bfc
LHJJosh <nicholasdare2@gmail.com>
2023-09-29 16:24:37 +0800

    Updates

---
+++ precache-manifest.5c6f6d3c2b11f892b8b3c13444d9342b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "878f21ad66b995de7b16c1949056556a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bfbbd8b22420e9507dd",
+    "url": "/static/css/2.3eb2a443.chunk.css"
+  },
+  {
+    "revision": "679fe73d3e480753d921",
+    "url": "/static/css/main.2f74bd79.chunk.css"
+  },
+  {
+    "revision": "4bfbbd8b22420e9507dd",
+    "url": "/static/js/2.e483c0ca.chunk.js"
+  },
+  {
+    "revision": "b8a26cc07166dc30cf9055bb104c91cd",
+    "url": "/static/js/2.e483c0ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "679fe73d3e480753d921",
+    "url": "/static/js/main.1507e3bd.chunk.js"
+  },
+  {
+    "revision": "1eb29b79da48b124f7b5",
+    "url": "/static/js/runtime-main.8ad5dc57.js"
+  }
+]);
